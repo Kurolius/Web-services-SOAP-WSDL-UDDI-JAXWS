bb771d7de4c242584a15f07dfe80a00a9e0301dd
Kurolius <ymajbar.pro@gmail.com>
2022-12-17 14:41:12 +0100

    Soap Client

--- SoapClientJava/src/main/java/org/enset/ClientWS.java
+++ SoapClientJava/src/main/java/org/enset/ClientWS.java
@@ -1,4 +1,4 @@
-package me.elmajni;
+package org.enset;
 
 import proxy.BanqueService;
 import proxy.BanqueWS;
--- wsenset/src/main/java/org/enset/ServeurJWS.java
+++ wsenset/src/main/java/org/enset/ServeurJWS.java
@@ -5,7 +5,7 @@ import org.enset.ws.BanqueService;
 
 public class ServeurJWS {
     public static void main(String[] args) {
-        Endpoint.publish("http://0.0.0.0:8085/",new BanqueService());
-        System.out.println("Web Service déployé sur l'adresse http://0.0.0.0:8085/!");
+        Endpoint.publish("http://0.0.0.0:9191/",new BanqueService());
+        System.out.println("Web Service déployé sur l'adresse http://0.0.0.0:9191/!");
     }
 }
